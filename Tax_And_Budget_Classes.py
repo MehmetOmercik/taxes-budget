6f1b1d3eb9d9f3101dd2b64a9f80e601968c9eef
Mehmet Omercik <mehmet.omercik@quantumgate.co.uk>
2023-02-25 16:53:26 +0000

    Refactoring variables to suit python's syntax

--- Tax_And_Budget_Classes.py
+++ Tax_And_Budget_Classes.py
@@ -8,74 +8,85 @@ class Taxes:
         self.gross_income = gross_income
         self.council_tax = council_tax
 
-    def incomeTax(self):
-        personalAllowance = 12570
+    def get_income_tax(self):
+        personal_allowance = 12570
         """ Calculating personal allowance if it goes over £100,000 
         (goes down by £1 for every £2 earned over)"""
         if self.gross_income > 100000:
-            personalAllowance -= (self.gross_income - 100000) / 2
-        if personalAllowance < 0: personalAllowance = 0
+            personal_allowance -= (self.gross_income - 100000) / 2
+        if personal_allowance < 0:
+            personal_allowance = 0
 
-        taxableIncome = self.gross_income - personalAllowance
-        if taxableIncome < 0: taxableIncome = 0
+        taxable_income = self.gross_income - personal_allowance
+        if taxable_income < 0:
+            taxable_income = 0
 
         # Income Tax
         # First tax band
         if self.gross_income <= 12570:
-            incomeTax = 0
+            income_tax = 0
             # Second tax band
         elif 12571 <= self.gross_income <= 50270:
-            incomeTax = 0.2 * (self.gross_income - 12570)  # 20% tax
+            income_tax = 0.2 * (self.gross_income - 12570)  # 20% tax
             # Third tax band
         elif 50271 <= self.gross_income <= 150000:
             # I put personal allowance variable here and below because there is a possibility of earning over £100000
-            incomeTax = 0.4 * (self.gross_income - 50271) + 0.2 * \
-                        (50270 - personalAllowance)
+            income_tax = 0.4 * (self.gross_income - 50271) + 0.2 * (
+                50270 - personal_allowance
+            )
             # Fourth tax band
         else:
-            incomeTax = 0.45 * (self.gross_income - 150000) + 0.4 * \
-                        (150000 - 50271) + 0.2 * (50270 - personalAllowance)
-        return [incomeTax, personalAllowance, taxableIncome]
-        # so incomeTax()[0] = incomeTax, incomeTax[1] = personalAllowance and incomeTax[2] = taxableIncome
+            income_tax = (
+                0.45 * (self.gross_income - 150000)
+                + 0.4 * (150000 - 50271)
+                + 0.2 * (50270 - personal_allowance)
+            )
+        return [income_tax, personal_allowance, taxable_income]
+        # so income_tax()[0] = income_tax, income_tax[1] = personal_allowance and income_tax[2] = taxable_income
 
-    def NItax(self):
+    def get_NI_tax(self):
         # NI Tax
         # First band
         if self.gross_income <= 12570:
-            NItax = 0
+            ni_tax = 0
         # Second band
         elif 12571 <= self.gross_income <= 50270:
-            NItax = 0.1375 * (self.gross_income - 12570)
+            ni_tax = 0.1375 * (self.gross_income - 12570)
         # Third band
         else:
-            NItax = 0.0325 * (self.gross_income - 50271) + 0.1375 * (50270 - 12570)
+            ni_tax = 0.0325 * (self.gross_income - 50271) + 0.1375 * (50270 - 12570)
 
-        return NItax
+        return ni_tax
 
-    def studentTax(self):
+    def get_student_tax(self):
         # Student loan repayment
         if self.gross_income > 27288:  # threshold for loan repayment
-            studentTax = (self.gross_income - 27288) * 0.09
+            student_tax = (self.gross_income - 27288) * 0.09
         else:
-            studentTax = 0
-        return studentTax
+            student_tax = 0
+        return student_tax
 
-    def totalTaxWS(self):
-        return Taxes.incomeTax(self)[0] + Taxes.NItax(self) + self.council_tax + Taxes.studentTax(self)
+    def get_total_tax(self):
+        return (
+            Taxes.get_income_tax(self)[0]
+            + Taxes.get_NI_tax(self)
+            + self.council_tax
+            + Taxes.get_student_tax(self)
+        )
 
-    def totalTaxWOS(self):
-        return Taxes.incomeTax(self)[0] + Taxes.NItax(self) + self.council_tax
+    # Total tax without student tax
+    def get_total_tax_wost(self):
+        return Taxes.get_income_tax(self)[0] + Taxes.get_NI_tax(self) + self.council_tax
 
-    def calculateIncomeWS(self):
-        return self.gross_income - Taxes.totalTaxWS(self)
+    def calculate_income(self):
+        return self.gross_income - Taxes.get_total_tax(self)
 
-    def calculateIncomeWOS(self):
-        return self.gross_income - Taxes.totalTaxWOS(self)
+    def calculate_income_wost(self):
+        return self.gross_income - Taxes.get_total_tax_wost(self)
 
 
 class Budget:
-    def __init__(self, rent, bills, food, toiletries,
-                 savings, miscellaneous, taxes):
+    def __init__(self, rent, bills, food, toiletries, savings, miscellaneous, taxes):
         self.rent = rent
         self.bills = bills
         self.food = food
@@ -84,15 +95,22 @@ class Budget:
         self.miscellaneous = miscellaneous
         self.taxes = taxes
 
-    def calculateMonthlyBudget(self):
-        return self.rent + self.food + self.toiletries + self.bills + self.savings + self.miscellaneous
+    def calculate_monthly_budget(self):
+        return (
+            self.rent
+            + self.food
+            + self.toiletries
+            + self.bills
+            + self.savings
+            + self.miscellaneous
+        )
 
-    def calculateYearlyBudget(self):
-        return 12 * Budget.calculateMonthlyBudget(self)
+    def calculate_yearly_budget(self):
+        return 12 * Budget.calculate_monthly_budget(self)
 
-    def calculateProfitWS(self):
-        return self.taxes.calculateIncomeWS() - Budget.calculateYearlyBudget(self)
+    def calculate_profit(self):
+        return self.taxes.calculate_income() - Budget.calculate_yearly_budget(self)
         # First term utilises a function in the taxes class by including taxes in the initialisation function
 
-    def calculateProfitWOS(self):
-        return self.taxes.calculateIncomeWOS() - Budget.calculateYearlyBudget(self)
+    def calculate_profit_wost(self):
+        return self.taxes.calculate_income_wost() - Budget.calculate_yearly_budget(self)
