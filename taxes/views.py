02dd9e9fb500862ba89fc8f1c829ad55b85a4c3d
Mehmet Omercik <mehmet.omercik@quantumgate.co.uk>
2023-03-05 23:07:49 +0000

    Added budget section

--- taxes/views.py
+++ taxes/views.py
@@ -2,14 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 import re
 
-from .forms import TaxesForm
+from .forms import TaxesForm, BudgetForm
 from .Tax_And_Budget_Classes import Taxes, Budget, round_twosf, round_twosf_month
 
 # Create your views here.
 def Home(request):
-    context = {"TaxesForm": TaxesForm()}
+    context = {"TaxesForm": TaxesForm(), "BudgetForm": BudgetForm()}
     if "taxes_form" in request.POST:
-        print(request.POST)
+        # print(request.POST)
+
+        """INCOME SECTION"""
 
         # Remove unneccesary characters
         gross_salary = re.sub("[£$,:;_]", "", request.POST["gross_salary"])
@@ -17,7 +19,7 @@ def Home(request):
 
         # Checking for anything other than int or float
         try:
-            float(gross_salary) and float(council_tax)
+            float(gross_salary) or float(council_tax)
         except:
             messages.error(
                 request,
@@ -30,7 +32,7 @@ def Home(request):
         gross_income = Taxes(gi, ct)
 
         # All calculations that need to be done
-        actions = [
+        actions_income = [
             ["Gross Income", gi],
             ["Tax-Free Allowance", gross_income.get_income_tax()[1]],
             ["Taxable Income", gross_income.get_income_tax()[2]],
@@ -42,17 +44,21 @@ def Home(request):
         # Checking whether the user selected the student loan or not and appends functions to actions
         if request.POST["student_loan"] == "True":
             # Forms process boolean as a string for some reason
-            actions.append(["Student Loan", gross_income.get_student_tax()])
-            actions.append(["Total Deductions", gross_income.get_total_tax()])
-            actions.append(["Total Income", gross_income.calculate_income()])
+            actions_income.append(["Student Loan", gross_income.get_student_tax()])
+            actions_income.append(["Total Deductions", gross_income.get_total_tax()])
+            actions_income.append(["Total Income", gross_income.calculate_income()])
         else:
-            actions.append(["Total Deductions", gross_income.get_total_tax_wost()])
-            actions.append(["Total Income", gross_income.calculate_income_wost()])
+            actions_income.append(
+                ["Total Deductions", gross_income.get_total_tax_wost()]
+            )
+            actions_income.append(
+                ["Total Income", gross_income.calculate_income_wost()]
+            )
 
-        final_results = []
+        final_results_income = []
 
-        for action in actions:
-            final_results.append(
+        for action in actions_income:
+            final_results_income.append(
                 [
                     action[0],
                     f"£{round_twosf(action[1])}",
@@ -60,6 +66,92 @@ def Home(request):
                 ]
             )
 
-        context = {"TaxesForm": TaxesForm(), "final_results": final_results}
+        """ BUDGET SECTION """
+        rent = (
+            re.sub("[£$,:;_]", "", request.POST["rent"])
+            if request.POST["rent"] != ""
+            else 0
+        )
+        bills = (
+            re.sub("[£$,:;_]", "", request.POST["bills"])
+            if request.POST["bills"] != ""
+            else 0
+        )
+
+        food = (
+            re.sub("[£$,:;_]", "", request.POST["food"])
+            if request.POST["food"] != ""
+            else 0
+        )
+
+        toiletries = (
+            re.sub("[£$,:;_]", "", request.POST["toiletries"])
+            if request.POST["toiletries"] != ""
+            else 0
+        )
+
+        savings = (
+            re.sub("[£$,:;_]", "", request.POST["savings"])
+            if request.POST["savings"] != ""
+            else 0
+        )
+
+        miscellaneous = (
+            re.sub("[£$,:;_]", "", request.POST["miscellaneous"])
+            if request.POST["miscellaneous"] != ""
+            else 0
+        )
+
+        # Checking for anything other than int or float
+        try:
+            float(rent) or float(bills) or float(food) or float(toiletries) or float(
+                savings
+            ) or float(miscellaneous)
+        except:
+            messages.error(
+                request,
+                "Incorrect input within budget section",
+            )
+            return redirect("home")
+        rent, bills, food, toiletries, savings, miscellaneous = (
+            float(rent),
+            float(bills),
+            float(food),
+            float(toiletries),
+            float(savings),
+            float(miscellaneous),
+        )
+        budget = Budget(
+            rent, bills, food, toiletries, savings, miscellaneous, gross_income
+        )
+
+        actions_budget = [["Yearly Budget", budget.calculate_yearly_budget()]]
+
+        if request.POST["student_loan"] == "True":
+            actions_budget.append(["Profit/Loss", budget.calculate_profit()])
+        else:
+            actions_budget.append(["Profit/Loss", budget.calculate_profit_wost()])
+
+        final_results_budget = []
+
+        for action in actions_budget:
+            final_results_budget.append(
+                [
+                    action[0],
+                    f"£{round_twosf(action[1])}",
+                    f"£{round_twosf_month(action[1])}",
+                ]
+            )
+        budget_hide = 0
+        if final_results_budget[0][1] == "£0.00":
+            budget_hide = 1
+
+        context = {
+            "TaxesForm": TaxesForm(),
+            "BudgetForm": BudgetForm(),
+            "final_results_income": final_results_income,
+            "final_results_budget": final_results_budget,
+            "budget_hide": budget_hide,
+        }
 
     return render(request, "taxes.html", context)
